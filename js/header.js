a025bb6e77bdc553c41ae25da3084963bfc9c414
2180945071 <2180945071@qq.com>
2017-08-03 00:29:09 +0700

    1

--- js/header.js
+++ js/header.js
@@ -1,3 +1,3 @@
 ﻿document.writeln("<h1 id=\'logo\'><a href=\'http://1djb.xyz\' title=\'\' target=\'_self\'><img src=\'images/1.1.png\' /></a></h1>");
-document.writeln(" <div id=\'topimg\'>导航现用域名：1djb.xyz</div>");
+document.writeln(" <div id=\'topimg\'>导航现用域名：https://177pic.github.io/</div>");
 document.writeln("<div class=\'favor\'><a onclick=\'addfavor(\'http://1djb.xyz\',\'导航\')\'>收藏本站</a>&nbsp;|&nbsp;<a href=\'/submit.html\' target=\'_blank\'>免费收录</a></div>");
